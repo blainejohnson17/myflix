a581cfe23905253259906562bc36372881637e7a
Blaine Johnson <blainejohnson17@gmail.com>
2013-11-04 14:29:55 -0700

    Adds video model validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,7 @@
 class Video < ActiveRecord::Base
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
+
+  validates :title, :presence => true
+  validates :description, :presence => true
 end
