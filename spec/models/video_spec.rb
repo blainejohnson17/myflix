723378bc6bdd502ccfcd0172ff920b24d5ff15a5
Blaine Johnson <blainejohnson17@gmail.com>
2013-11-04 23:12:27 -0700

    Adds test for video has many categories on video model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,4 +7,13 @@ describe Video do
     Video.first.title.should == 'Gooneys'
     Video.first.description.should == 'Movie about kids, pirates and treasure'
   end
+
+  it "has many categories" do
+    video = Video.create(title: 'Ghost Busters', description: 'Ghost movie')
+    
+    (1..10).each do |i|
+      video.categories << Category.create(name: "Category #{1}")
+    end
+    video.categories.count.should == 10
+  end
 end
