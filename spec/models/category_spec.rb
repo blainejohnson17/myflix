83235acd9403bd74181fdffa16f1d342df398c65
Blaine Johnson <blainejohnson17@gmail.com>
2013-11-06 02:43:51 -0700

    Refactors tests with shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_helper'
 
-describe Category do 
-  it "saves itself" do
-    category = Category.new(name: "Comedy")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    comedy = Category.create(name: "Comedy")
-    gooneys = Video.create(title: "Gooneys", description: "Pirate Movie", categories: [comedy])
-    space_balls = Video.create(title: "Space Balls", description: "Space Movie", categories: [comedy])
-    expect(comedy.videos).to eq([gooneys, space_balls])
-  end
-
-  it "does not save a category without a name" do
-    category = Category.create()
-    expect(Category.count).to eq(0)
-  end
+describe Category do
+  it { should have_many(:videos) }
+  it { should validate_presence_of(:name) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,26 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    gooneys = Video. new(title: 'Gooneys', description: 'Movie about kids, pirates and treasure')
-    gooneys.save
-    expect(Video.first).to eq(gooneys)
-  end
-
-  it "has many categories" do
-    gooneys = Video.create(title: "Gooneys", description: "Pirate Movie")
-    action = Category.create(name: "action", videos: [gooneys])
-    comedy = Category.create(name: "comedy", videos: [gooneys])
-    expect(gooneys.categories).to eq([action, comedy])
-  end
-
-  it "does not save a video without a title" do
-    video = Video.create(description: "Awesome Video!")
-    expect(Video.count).to eq(0)
-  end
-
-  it "does not save a video without description" do
-    video = Video.create(title: "Gooneys")
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
