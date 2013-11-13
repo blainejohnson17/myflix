5ef7dc4d11e286e0081e432b09c3e722d99b21a3
Blaine Johnson <blainejohnson17@gmail.com>
2013-11-13 14:16:11 -0700

    adds sessions controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe UsersController do
+
   describe "GET #new" do
+    
     it "assigns new User to @user" do
       get :new
       expect(assigns(:user)).to be_a_new(User)
