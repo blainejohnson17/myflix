e44c1057e1a4f9b4dea34dc869992eaf8fdf5e53
Blaine Johnson <blainejohnson17@gmail.com>
2013-11-14 00:49:04 -0700

    Adds reviews for videos

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -12,6 +12,14 @@ describe VideosController do
         get :show, id: video
         expect(assigns(:video)).to eq(video)
       end
+
+      it "assigns the reviews for the requested video to @reviews" do
+        video = Fabricate(:video)
+        review1 = Fabricate(:review, video: video)
+        review2 = Fabricate(:review, video: video)
+        get :show, id: video
+        expect(assigns(:reviews)).to match_array([review1, review2])
+      end
     end
 
     context "with unauthenticated user" do
