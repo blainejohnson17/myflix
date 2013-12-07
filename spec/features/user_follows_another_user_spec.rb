86436bfa3e92c51d99a288120bbef982c7cc2882
Blaine Johnson <blainejohnson17@gmail.com>
2013-12-06 17:56:26 -0700

    Refactors following user feature spec

--- spec/features/user_follows_another_user_spec.rb
+++ spec/features/user_follows_another_user_spec.rb
@@ -15,18 +15,32 @@ feature "User follows another user" do
 
     click_video_on_home_page(gooneys)
 
-    click_link bob.full_name
+    follow_user(bob)
+    expect_following_user(bob)
 
-    click_link "Follow"
+    unfollow_user
+    expect_unfollow_user(bob)
+  end
 
+  def follow_user(user)
+    click_link user.full_name
+    click_link "Follow"    
+  end
+
+  def expect_following_user(user)
     within("section.people") do
-      expect(page).to have_content(bob.full_name)
-    end
-
-    find("a[href='/relationships/1']").click
-
-    within("section.people") do
-      expect(page).not_to have_content(bob.full_name)
+      expect(page).to have_content(user.full_name)
     end
   end
+
+  def unfollow_user
+    find("a[href='/relationships/1']").click
+  end
+
+  def expect_unfollow_user(user)
+    within("section.people") do
+      expect(page).not_to have_content(user.full_name)
+    end
+  end
+
 end
